51134fdbca817d8e335e58ae185bab950499d8e9
Matt McAlister <matt.mcalister93@gmail.com>
2017-10-27 19:17:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.first do |word| 
+  array.first do |word|
     cheese_types.include?(word)
   end
 end
